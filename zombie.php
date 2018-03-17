85bde33817a9b3af7218e6a2a117d16e85180d5d
YoungNotTheOld1 <ylee8575@gmail.com>
2018-03-16 21:20:54 -0700

    Created navbar for ecommerce site.

---
+++ zombie.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+  <meta charset="utf-8" />
+  <title>Zombie Emporium</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/main.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1,user-scaleable=no">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-default navbar-fixed-top" id="navbar">
+    <div class="container">
+      <a href="/zombie-emporium/zombie.php" class="navbar-brand">Zombie Emporium</a>
+      <ul class="nav navbar-nav">
+        <!-- Drop Down Menu -->
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="text">Clothing<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Shirts</a></li>
+            <li><a href="#">Pants</a></li>
+            <li><a href="#">Shoes</a></li>
+            <li><a href="#">Accessories</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="text">Survival<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#"></a></li>
+            <li><a href="#">Basic Needs</a></li>
+            <li><a href="#">Tools</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="text">Weapons<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Silent but Deadly</a></li>
+            <li><a href="#">Guns Blazin'</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</body>
+</html>
